c3925f4bd9bfbdab76c2524a530f749a96bb89d7
MiataMyke <Myke428@gmail.com>
2019-08-09 01:44:14 -0500

    initial commit

---
+++ src/main/java/Liftoff/climbwithme/ClimbWithMeApplication.java
@@ -0,0 +1,13 @@
+package Liftoff.climbwithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbWithMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Liftoff/climbwithme/ClimbWithMeApplicationTests.java
@@ -0,0 +1,16 @@
+package Liftoff.climbwithme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClimbWithMeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
