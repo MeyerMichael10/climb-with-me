b4d6110cfc50a93e25aae4e5b81e54aff1f87e9a
MiataMyke <Myke428@gmail.com>
2019-08-14 21:49:48 -0500

    Added Role class and related repository

---
+++ src/main/java/Liftoff/climbwithme/models/data/RoleDao.java
@@ -0,0 +1,7 @@
+package Liftoff.climbwithme.models.data;
+
+import Liftoff.climbwithme.models.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleDao extends CrudRepository<Role, Integer> {
+}
